526aa6aee6f4212185e6f090c02deaa69e32618c
Brian Mooney <brian_satchwannabe@hotmail.com>
2018-12-03 23:37:16 +0000

    Submit Day 3

--- src/main/scala/ie/irishshagua/advent_of_code/Day3.scala
+++ src/main/scala/ie/irishshagua/advent_of_code/Day3.scala
@@ -9,8 +9,21 @@ object Day3 extends App {
 
   val ValidClaimPattern = "#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)".r.pattern
 
-  val input = Source.fromResource("day3/input").getLines().flatMap(toClaim)
-  input.foreach(println)
+  require(multiRequestedArea(List("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2").flatMap(toClaim)) == 4)
+  require(uniqueClaim(List("#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2").flatMap(toClaim)).map(_.id).contains(3))
+
+  val input = Source.fromResource("day3/input").getLines().flatMap(toClaim).toList
+  println(s"Total Duplicated Requested Area: ${multiRequestedArea(input)}")
+  println(s"Unique Claim: ${uniqueClaim(input)}")
+
+  def multiRequestedArea(claims: List[Claim]): Int = {
+    val allClaimedCoords = claims.foldLeft(List[(Int, Int)]()) { case (accum, claim) =>
+      areaFromClaim(claim) ::: accum
+    }
+    val multiRequestedCoords = allClaimedCoords.diff(allClaimedCoords.distinct).distinct
+
+    multiRequestedCoords.size
+  }
 
   def toClaim(detail: String): Option[Claim] = {
     val matcher = ValidClaimPattern.matcher(detail)
@@ -26,4 +39,17 @@ object Day3 extends App {
       ))
     } else None
   }
+
+  // Ridiculously inefficient... 🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢:-/
+  def areaFromClaim(claim: Claim): List[(Int, Int)] = (for {
+      x <- claim.x until (claim.x + claim.dimensions.width)
+      y <- claim.y until (claim.y + claim.dimensions.height)
+    } yield (x, y)).toList.distinct
+
+  def uniqueClaim(claims: List[Claim]): Option[Claim] = claims.find { claim =>
+      !claims.filterNot(_ == claim).exists { intersects(claim, _) }
+    }
+
+  def intersects(claim1: Claim, claim2: Claim): Boolean =
+    areaFromClaim(claim1).intersect(areaFromClaim(claim2)).nonEmpty
 }
